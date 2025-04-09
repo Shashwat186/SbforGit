3812e4ed2ec6c3d8deb7c0cbc8c43520fb852a0a
Chauhan <shashwatcha10@gmail.com>
2025-04-09 18:00:54 +0530

    intial commit

---
+++ src/main/java/sb/for_git/SbforGitApplication.java
@@ -0,0 +1,13 @@
+package sb.for_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbforGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbforGitApplication.class, args);
+	}
+
+}
